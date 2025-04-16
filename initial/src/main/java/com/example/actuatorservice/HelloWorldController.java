a932c9e0db0b0840364045f4fb499aa2fc9a7004
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-16 18:44:39 -0400

    initialized application and created greeting class and creating controller to handle endpoints

---
+++ initial/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -0,0 +1,9 @@
+package com.example.actuatorservice;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+}
