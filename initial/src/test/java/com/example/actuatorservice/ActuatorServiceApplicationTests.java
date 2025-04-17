938805837e6c076acbc1b333e6df11d85e68223f
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-16 22:06:44 -0400

    final commit complete

--- initial/src/test/java/com/example/actuatorservice/ActuatorServiceApplicationTests.java
+++ initial/src/test/java/com/example/actuatorservice/ActuatorServiceApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.actuatorservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+
 class ActuatorServiceApplicationTests {
 
 	@Test
